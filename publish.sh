c0cef3fcb487b19543e6e5f52e1016a39d4e2fa2
Mikael Rinnetmäki <mikael@sensotrend.com>
2024-10-22 10:40:00 +0300

    Make publish more robust
    This takes more time to build, but is more robust.
    We want to empty the temp folder in order to not make the resulting zip archive too big. Everything in temp seems to be copied there.
    Also, make sure to run _genonce at least one, so that the version is updated.

--- publish.sh
+++ publish.sh
@@ -1,1 +1,3 @@
+rm -rf temp/
+./_genonce.sh
 java -jar input-cache/publisher.jar -go-publish -source $(pwd) -web https://hl7.fi/fhir -registry $(pwd)/../ig-registry/fhir-ig-list.json -history $(pwd)/../fhir-ig-history-template/ -templates $(pwd)/../history-template-fhir-fi -temp $(pwd)/temp
